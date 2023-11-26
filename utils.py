3ee9b22099434ccc0a6d391f06462d4319a449c6
albertsalikhov <bert56@yandex.ru>
2023-11-26 17:53:12 +0300

    add README.md

--- utils.py
+++ utils.py
@@ -4,28 +4,33 @@ from datetime import datetime
 
 
 def get_data():
-    """Получаем список словарей"""
+    """Получаем данные из файла"""
     with open('operations.json', 'r', encoding='utf-8') as file:
         data = json.load(file)
         return data
 
 
 def get_filtered_data(data):
+    """Фильтруем, получаем только выполненные транзакции "EXECUTED"""
     data = [x for x in data if "state" in x and x["state"] == "EXECUTED"]
     return data
 
 
 def get_last_values(data, count_last_values):
+    """Получаем последние 5 значений транзакций"""
     data = sorted(data, key=lambda x: x["date"], reverse=True)
     data = data[:5]
     return data
 
 
 def get_formatted_data(data):
+    """Форматирует и выводит список строк в требуемом формате """
     formatted_data = []
     for row in data:
         print(row['date'])
         date = datetime.strptime(row["date"], __format="%Y-%m-%dT%H:%M:%S.%f").strftime("%d.%m.%Y")
+        print(date)
+
 
 
 
